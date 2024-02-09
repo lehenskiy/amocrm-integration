5ece6567c5c3a11692df9ba80e1446ffda3f446d
Sheetov Alexey <50871152+lehenskiy@users.noreply.github.com>
2024-02-09 18:30:49 +0700

    Create RequisitionForm.php

---
+++ src/Requisition/Create/RequisitionForm.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Requisition\Create;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RequisitionForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('phoneNumber', TelType::class)
+            ->add('price', MoneyType::class, [
+                'currency' => 'RUB', // https://en.wikipedia.org/wiki/ISO_4217
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => RequisitionDTO::class,
+            'empty_data' => new RequisitionDTO(),
+        ]);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return '';
+    }
+}
